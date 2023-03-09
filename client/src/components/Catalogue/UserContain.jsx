03e445549b6e0190e659c3275984df9e9bb25b5f
Marcylove <danaeslo2004@gmail.com>
2023-03-09 11:41:59 -0500

    Rediseño de perfil 2.0

---
+++ client/src/components/Catalogue/UserContain.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { UserCard } from './UserCard'
+
+export const UserContain = () => {
+  return (
+    <section className="mega-user-contain">
+        <UserCard/>
+        <UserCard/>
+        <UserCard/>
+        <UserCard/>
+        <UserCard/>
+        <UserCard/>
+    </section>
+  )
+}
---
+++ client/src/components/Profile/ContainerPost.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Post } from './Post'
+
+export const ContainerPost = () => {
+  return (
+    <section className="container-post">
+        <Post/>
+        <Post/>
+        <Post/>
+    </section>
+  )
+}
